8cfee90cf29e04c35629052d7e5f1be5033aa15f
Denis Pujdak <denis.pujdak@hotmail.com>
2021-02-25 13:10:27 +0000

    init

---
+++ .electron-builder.js
@@ -0,0 +1,17 @@
+/**
+* @type {import('electron-builder').Configuration}
+* @see https://www.electron.build/configuration/configuration
+*/
+module.exports = {
+    appId: "com.el_test.cmu",
+    productName: "Electron Test CMU",
+    copyright: "Copyright © year ${author}",
+    win: {
+      "target": "nsis",
+      "icon": "public/favicon.ico"
+    },
+    rpm: {
+      "category": "Other"
+    }
+  }
+  
